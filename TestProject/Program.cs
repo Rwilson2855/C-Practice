f24e4e63205b760bcaa2277a361c540070e31649
Ryan Wilson <rwilson2855@gmail.com>
2026-03-06 15:03:32 -0600

    monster game practice

--- TestProject/Program.cs
+++ TestProject/Program.cs
@@ -1,8 +1,29 @@
-﻿Random random = new Random ();
-int current = 0;
+﻿/*
+You must use either the do-while statement or the while statement as an outer game loop.
+The hero and the monster start with 10 health points.
+All attacks are a value between 1 and 10.
+The hero attacks first.
+Print the amount of health the monster lost and their remaining health.
+If the monster's health is greater than 0, it can attack the hero.
+Print the amount of health the hero lost and their remaining health.
+Continue this sequence of attacking until either the monster's health or hero's health is zero or less.
+Print the winner.
+*/
+Random random = new Random();
+int heroHealth = 10;
+int monsterHealth = 10;
 
 do
 {
-    current = random.Next(1, 11);
-    Console.WriteLine(current);
-} while (current != 7);
+    int heroDamageDealt = random.Next (1, 11);
+    int monsterDamageDealt = random.Next (1, 11);
+    
+
+    monsterHealth -= heroDamageDealt;
+    Console.WriteLine($"Monster  takes {heroDamageDealt} and has {monsterHealth}");
+    if (monsterHealth <= 0) continue;
+    heroHealth -= monsterDamageDealt;
+    Console.WriteLine($"Hero takes {monsterDamageDealt} and has {heroHealth}");
+    
+} while (monsterHealth > 0 && heroHealth > 0);
+Console.WriteLine(heroHealth > monsterHealth ? "Hero wins!" : "Monster wins!");
--- TestProject/obj/Debug/net9.0/TestProject.AssemblyInfo.cs
+++ TestProject/obj/Debug/net9.0/TestProject.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TestProject")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7be95c53a958d0c24179463164de790614b9fa78")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+102a9236c47dfe94272c26eecda3992b750d8500")]
 [assembly: System.Reflection.AssemblyProductAttribute("TestProject")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TestProject")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
