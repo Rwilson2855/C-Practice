b4755a57606216ccbf7e445c6177235d5b095c06
Ryan Wilson <rwilson2855@gmail.com>
2026-04-03 15:50:18 -0500

    data type practice

--- TestProject/Program.cs
+++ TestProject/Program.cs
@@ -1,30 +1,8 @@
-﻿string? readResult;
-string valueEntered = "";
-int numericValue = 0;
-bool validNumber = false;
+﻿int[] ref_A= new int[1];
+ref_A[0] = 2;
+int[] ref_B = ref_A;
+ref_B[0] = 5;
 
-
-
-
-Console.WriteLine ("Please enter a value between 5 and 10");
-do
-{
-    readResult = Console.ReadLine();
-    if (readResult != null)
-    {
-        valueEntered = readResult;
-    }
-    validNumber = int.TryParse(valueEntered, out numericValue);
-        if (validNumber == true)
-    {
-        if (numericValue <= 5 || numericValue >= 10)
-        {validNumber = false;}
-        Console.WriteLine($"You entered {numericValue}. Please enter a value between 5 and 10");
-    }
-    else
-    {
-        Console.WriteLine("Sorry, you entered an invalid number, please try again");
-    }}
- while (validNumber == false);
- Console.WriteLine ($"Your input value ({numericValue}) has been accepted.");
- readResult = Console.ReadLine ();
+Console.WriteLine("--Reference Types--");
+Console.WriteLine($"ref_A[0]: {ref_A[0]}");
+Console.WriteLine($"ref_B[0]: {ref_B[0]}");
