22e915df9275a8b18b2aeebfe6bdf5d78f1d16be
Ryan Wilson <rwilson2855@gmail.com>
2026-04-10 15:12:21 -0500

    stuff

--- TestProject/Program.cs
+++ TestProject/Program.cs
@@ -1,8 +1,38 @@
-﻿int[] ref_A= new int[1];
-ref_A[0] = 2;
-int[] ref_B = ref_A;
-ref_B[0] = 5;
+﻿/*string[] values = { "12.3", "45", "ABC", "11", "DEF" };
 
-Console.WriteLine("--Reference Types--");
-Console.WriteLine($"ref_A[0]: {ref_A[0]}");
-Console.WriteLine($"ref_B[0]: {ref_B[0]}");
+decimal total = 0m;
+string message = "";
+
+foreach (var value in values)
+{
+    decimal number; // stores the TryParse "out" value
+    if (decimal.TryParse(value, out number))
+    {
+        total += number;
+    }
+    else
+    {
+        message += value;
+    }
+}
+
+Console.WriteLine($"Message: {message}");
+Console.WriteLine($"Total: {total}"); */
+
+using System.IO.Pipelines;
+int value1 = 11;
+decimal value2 = 6.2m;
+float value3 = 4.3f;
+
+int result1 = Convert.ToInt32(value1/value2); 
+
+
+Console.WriteLine($"Divide value1 by value2, display the result as an int: {result1}");
+decimal result2 = value2/(decimal)value3;
+
+
+Console.WriteLine($"Divide value2 by value3, display the result as a decimal: {result2}");
+float result3 = value3/value1;
+
+
+Console.WriteLine($"Divide value3 by value1, display the result as a float: {result3}");
--- TestProject/obj/Debug/net9.0/TestProject.AssemblyInfo.cs
+++ TestProject/obj/Debug/net9.0/TestProject.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TestProject")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2c2265316b67a35602d9d3003e6c3f488cc796e8")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b4755a57606216ccbf7e445c6177235d5b095c06")]
 [assembly: System.Reflection.AssemblyProductAttribute("TestProject")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TestProject")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
